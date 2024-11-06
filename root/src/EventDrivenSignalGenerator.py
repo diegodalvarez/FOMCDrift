a25cfdbdde3c018649fee93fb9b75788f7cbf2b0
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2024-11-06 09:00:00 -0500

    Add files via upload

---
+++ root/src/EventDrivenSignalGenerator.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 12:41:16 2024
+
+@author: Diego
+"""
+
+import os
+import sys
+import pandas as pd
+from   FOMCDataPrep import DataPrep
+
+class EventDrivenSignalGenerator(DataPrep):
+    
+    def __init__(self):
+        
+        super().__init__()
+        self.fdtr_event = r"C:\Users\Diego\Desktop\app_prod\BBGEvent\data\event"
+        
+    def _fill_days(self, df: pd.DataFrame, before: int, after: int, verbose: bool = False) -> pd.DataFrame:
+        
+        day_count = abs(before) + after + 1
+        days      = [i + before for i in range(day_count)]
+        try: 
+            
+            df = df.assign(day = days)
+            return df
+            
+        except: 
+            if verbose == True: print("failed at {} {}".format(df.name, df.date.min()))
+    
+    def get_ordered_window(
+            self, 
+            df     : pd.DataFrame, 
+            before : int = -1, 
+            after  : int = 1,
+            verbose: bool = False) -> pd.DataFrame: 
+        
+        df_spx = (self.get_equity_futures().query(
+            "security == 'ES1'")
+            [["date"]].
+            assign(date = lambda x: pd.to_datetime(x.date).dt.date).
+            sort_values("date"))
+        
+        df_fdtr_dates = (pd.read_parquet(
+            path   = os.path.join(self.fdtr_event, "FDTR.parquet"),
+            engine = "pyarrow").
+            assign(date = lambda x: pd.to_datetime(x.date).dt.date).
+            drop(columns = ["security", "ECO_RELEASE_DT"]).
+            assign(indicator = True))
+        
+        min_date = max([df_spx.date.min(), df_fdtr_dates.date.min()])
+        
+        df_tmp = (df_fdtr_dates.query(
+            "date >= @min_date").
+            sort_values("date").
+            merge(right = df_spx.query("date >= @min_date"), how = "outer", on = ["date"]).
+            assign(date = lambda x: pd.to_datetime(x.date).dt.date).
+            sort_values("date").
+            assign(
+                prev_date = lambda x: x.date.shift(-before),
+                post_date = lambda x: x.date.shift(-after)).
+            dropna().
+            reset_index(drop = True).
+            reset_index().
+            rename(columns = {"index": "event"}).
+            assign(event = lambda x: x.event + 1))
+
+        df_out = pd.DataFrame()
+        for i, row in df_tmp.iterrows():
+            
+            start_date, end_date = row["prev_date"], row["post_date"]
+            df_tmp = (df.query(
+                "@start_date <= date <= @end_date").
+                assign(date = lambda x: pd.to_datetime(x.date).dt.date).
+                groupby("variable").
+                apply(self._fill_days, before, after, verbose).
+                reset_index(drop = True).
+                assign(event = row["event"]))
+            
+            df_out = pd.concat([df_out, df_tmp])
+            
+        return df_out
---
+++ root/src/FOMCDataPrep.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 07:02:30 2024
+
+@author: Diego
+"""
+
+import os
+import pandas as pd
+import datetime as dt
+import pandas_datareader as web
+
+class DataPrep:
+    
+    def __init__(self):
+        
+        self.data_path = os.path.join(os.getcwd().split("\\root")[0], "data", "RawData")
+        self.bbg_path  = r"C:\Users\Diego\Desktop\app_prod\BBGData"
+        self.bbg_fut   = r"C:\Users\Diego\Desktop\app_prod\BBGFuturesManager"
+        
+        if os.path.exists(self.data_path) == False: os.makedirs(self.data_path)
+        
+    def _get_fut_rtn(self, df: pd.DataFrame) -> pd.DataFrame: 
+        
+        return(df.sort_values(
+            "date").
+            assign(
+                PX_diff = lambda x: x.PX_LAST.diff(),
+                PX_pct  = lambda x: x.PX_LAST.pct_change()).
+            dropna())
+    
+    def _get_yld_diff(self, df: pd.DataFrame) -> pd.DataFrame: 
+        
+        return(df.sort_values(
+            "date").
+            assign(val_diff = lambda x: x.value.diff()).
+            dropna())
+        
+    def get_tsy_yields(self, verbose: bool = False) -> pd.DataFrame:
+        
+        path = os.path.join(self.data_path, "TSYFredYields.parquet")
+        try:
+            
+            if verbose == True: print("Searching for Treasury data")
+            df_out = pd.read_parquet(path = path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+            
+        except:
+            
+            tsy_tickers = [
+                "DGS1MO", "DGS3MO", "DGS1", "DGS2", "DGS5", "DGS7", "DGS10", 
+                "DGS20", "DGS30"]
+            
+            start_date = dt.date(year = 1900, month = 1, day = 1)
+            end_date   = dt.date.today()
+            
+            if verbose == True: print("Collecting Data")
+            df_out = (web.DataReader(
+                name        = tsy_tickers,
+                data_source = "fred",
+                start       = start_date,
+                end         = end_date).
+                reset_index().
+                rename(columns = {"DATE": "date"}).
+                melt(id_vars = "date").
+                dropna().
+                groupby("variable").
+                apply(self._get_yld_diff).
+                reset_index(drop = True))
+            
+            df_out.to_parquet(path = path, engine = "pyarrow")
+            
+        return df_out
+    
+    def get_sentiment(self, verbose: bool = False) -> pd.DataFrame:
+        
+        read_path = os.path.join(self.data_path, "SentimentData.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find Sentiment data")
+            df_out = pd.read_parquet(path = read_path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+            
+        except:
+            
+            if verbose == True: print("Collecting data")
+        
+            tickers_path = os.path.join(self.bbg_path, "root", "BBGTickers.xlsx")
+            keywords     = ["Decomposition", "Federal"]
+            
+            df_tickers = (pd.read_excel(
+                io = tickers_path, sheet_name = "tickers").
+                assign(
+                    tmp1 = lambda x: x.Description.str.split(" ").str[0],
+                    tmp2 = lambda x: x.Description.str.split(" ").str[1],
+                    tmp3 = lambda x: x.Description.str.split(" ").str[2]).
+                query("tmp1 == 'Bloomberg' & tmp2 == 'Economics' & tmp3 == @keywords")
+                [["Security", "Description"]].
+                rename(columns = {"Security": "security"}))
+            
+            tickers = (df_tickers.assign(
+                ticker = lambda x: x.security.str.split(" ").str[0]).
+                ticker.
+                drop_duplicates().
+                to_list())
+            
+            file_paths = [
+                os.path.join(self.bbg_path, "data", ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_raw = (pd.read_parquet(
+                path = file_paths, engine = "pyarrow").
+                drop(columns = ["variable"]).
+                assign(ending = lambda x: x.security.str[0]))
+            
+            df_ben, df_a = df_raw.query("ending != 'A'"), df_raw.query("ending == 'A'")
+            
+            df_a_prep = (df_a.drop(
+                columns = ["ending"]).
+                pivot(index = "date", columns = "security", values = "value").
+                cumsum().
+                reset_index().
+                melt(id_vars = "date"))
+            
+            df_out = (pd.concat(
+                [df_ben.drop(columns = ["ending"]), df_a_prep]).
+                merge(right = df_tickers, how = "inner", on = ["security"]).
+                assign(
+                    security = lambda x: x.security.str.split(" ").str[0],
+                    date     = lambda x: pd.to_datetime(x.date).dt.date))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = read_path, engine = "pyarrow")
+        
+        return df_out
+    
+    def get_tsy_futures(self, verbose = False) -> pd.DataFrame: 
+        
+        read_path = os.path.join(self.data_path, "TreasuryFutures.parquet")
+        try:
+            
+            if verbose == True: print("Looking for Treasury data")
+            df_out = pd.read_parquet(path = read_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+        
+            if verbose == True: print("Coouldn't find Data")
+            tickers = ["TU", "TY", "US", "FV", "UXY", "WN"]
+            
+            px_paths = [
+                os.path.join(self.bbg_fut, "data", "PXFront", file + ".parquet")
+                for file in tickers]
+            
+            deliv_paths = [
+                os.path.join(self.bbg_fut, "data", "BondDeliverableRisk", file + ".parquet")
+                for file in tickers]
+            
+            df_px = (pd.read_parquet(
+                path = px_paths, engine = "pyarrow").
+                groupby("security").
+                apply(self._get_fut_rtn).
+                reset_index(drop = True))
+            
+            df_deliv = (pd.read_parquet(
+                path = deliv_paths, engine = "pyarrow").
+                pivot(index = ["date", "security"], columns = "variable", values = "value").
+                reset_index().
+                rename(columns = {
+                    "FUT_EQV_CNVX_NOTL"            : "FUT_CNVX",
+                    "CONVENTIONAL_CTD_FORWARD_FRSK": "CTD_DUR"}).
+                dropna())
+            
+            df_out = (df_px.merge(
+                right = df_deliv, how = "inner", on = ["date", "security"]).
+                assign(PX_bps = lambda x: x.PX_diff / x.CTD_DUR))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = read_path, engine = "pyarrow")
+        
+        return df_out
+    
+    def get_equity_futures(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        read_path = os.path.join(self.data_path, "EquityFutures.parquet")
+        try:
+            
+            if verbose == True: print("Looking for Equity Futures data")
+            df_out = pd.read_parquet(path = read_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+        
+            if verbose == True: print("Couldn't equity future data")
+            tickers  = ["ES", "UX"]
+            px_paths = [
+                os.path.join(self.bbg_fut, "data", "PXFront", file + ".parquet")
+                for file in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = px_paths, engine = "pyarrow").
+                assign(
+                    security = lambda x: x.security.str.split(" ").str[0],
+                    date     = lambda x: pd.to_datetime(x.date).dt.date).
+                groupby("security").
+                apply(self._get_fut_rtn).
+                reset_index(drop = True))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = read_path, engine = "pyarrow")
+        
+        return df_out
+    
+    def get_fed_funds(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        read_path = os.path.join(self.data_path, "FFRate.parquet")
+        try:
+            
+            if verbose == True: print("Looking for Fed Funds Data data")
+            df_out = pd.read_parquet(path = read_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+        
+            if verbose == True: print("Couldn't Fed Funds data")
+            
+            file_path = os.path.join(self.bbg_path, "data", "FDTR.parquet")
+            df_out = (pd.read_parquet(
+                path = file_path, engine = "pyarrow").
+                assign(
+                    date     = lambda x: pd.to_datetime(x.date).dt.date,
+                    security = lambda x: x.security.str.split(" ").str[0],
+                    val_diff = lambda x: x.value.diff()).
+                dropna().
+                drop(columns = ["val_diff", "variable", "security"]).
+                rename(columns = {"value": "FDTR"}))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = read_path, engine = "pyarrow")
+            
+        return df_out
+    
+    
+    
+    
+def main():
+    
+    data_prep = DataPrep()
+    data_prep.get_sentiment(verbose = True)
+    data_prep.get_fed_funds(verbose = True)
+    data_prep.get_tsy_yields(verbose = True)
+    data_prep.get_tsy_futures(verbose = True)
+    data_prep.get_equity_futures(verbose = True)
+
+#if __name__ == "__main__": main()
